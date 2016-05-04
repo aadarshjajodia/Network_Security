a65c53277e0dd4f877148efdae5bb29fa92bae03
Aadarsh Jajodia <aadarshjajodia@gmail.com>
2016-05-04 03:30:21 -0400

    Removing unncessary code

--- hw4/mysniffer.c
+++ hw4/mysniffer.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -12,7 +11,6 @@
 #include <netinet/if_ether.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <time.h>
 
 /* default snap length (maximum bytes per packet to capture) */
 #define SNAP_LEN 1518
@@ -21,11 +19,6 @@
 #define SIZE_ETHERNET 14
 #define SIZE_DNS_ANSWER_DATA_1 16
 #define SIZE_DNS_HEADER 12
-
-#define ARP_REQUEST 1   /* ARP Request             */ 
-#define ARP_REPLY 2     /* ARP Reply               */ 
-
-int32_t gmt2local(time_t t);
 
 /* IP header */
 struct sniff_ip {
@@ -80,131 +73,8 @@ struct dns_answer {
 #define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
 #define TH_OFF(th)      		((th)->th_off)
 
-struct icmphdr
-{
-  u_short type;		/* message type */
-  u_short code;		/* type sub-code */
-  u_int checksum;
-  u_long restofHeader;
-};
-
-#define ICMP_ECHOREPLY		0	/* Echo Reply			*/
-#define ICMP_ECHO		8	/* Echo Request			*/
-
-int tflag = 1;
-int32_t thiszone;
-/* TCP header */
-
-/* ARP Header, (assuming Ethernet+IPv4)            */ 
-#define ARP_REQUEST 1   /* ARP Request             */ 
-#define ARP_REPLY 2     /* ARP Reply               */ 
-
 void
 got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
-
-void
-print_payload(const u_char *payload, int len);
-
-void
-print_hex_ascii_line(const u_char *payload, int len, int offset);
-
-/*
- * print data in rows of 16 bytes: offset   hex   ascii
- *
- * 00000   47 45 54 20 2f 20 48 54  54 50 2f 31 2e 31 0d 0a   GET / HTTP/1.1..
- */
-void
-print_hex_ascii_line(const u_char *payload, int len, int offset)
-{
-
-	int i;
-	int gap;
-	const u_char *ch;
-
-	/* offset */
-	printf("%05d   ", offset);
-	
-	/* hex */
-	ch = payload;
-	for(i = 0; i < len; i++) {
-		printf("%02x ", *ch);
-		ch++;
-		/* print extra space after 8th byte for visual aid */
-		if (i == 7)
-			printf(" ");
-	}
-	/* print space to handle line less than 8 bytes */
-	if (len < 8)
-		printf(" ");
-	
-	/* fill hex gap with spaces if not full line */
-	if (len < 16) {
-		gap = 16 - len;
-		for (i = 0; i < gap; i++) {
-			printf("   ");
-		}
-	}
-	printf("   ");
-	
-	/* ascii (if printable) */
-	ch = payload;
-	for(i = 0; i < len; i++) {
-		if (isprint(*ch))
-			printf("%c", *ch);
-		else
-			printf(".");
-		ch++;
-	}
-
-	printf("\n");
-
-return;
-}
-
-/*
- * print packet payload data (avoid printing binary data)
- */
-void
-print_payload(const u_char *payload, int len)
-{
-
-	int len_rem = len;
-	int line_width = 16;			/* number of bytes per line */
-	int line_len;
-	int offset = 0;					/* zero-based offset counter */
-	const u_char *ch = payload;
-
-	if (len <= 0)
-		return;
-
-	/* data fits on one line */
-	if (len <= line_width) {
-		print_hex_ascii_line(ch, len, offset);
-		return;
-	}
-
-	/* data spans multiple lines */
-	for ( ;; ) {
-		/* compute current line length */
-		line_len = line_width % len_rem;
-		/* print line */
-		print_hex_ascii_line(ch, line_len, offset);
-		/* compute total remaining */
-		len_rem = len_rem - line_len;
-		/* shift pointer to remaining bytes to print */
-		ch = ch + line_len;
-		/* add offset */
-		offset = offset + line_width;
-		/* check if we have line width chars or less */
-		if (len_rem <= line_width) {
-			/* print last line and get out */
-			print_hex_ascii_line(ch, len_rem, offset);
-			break;
-		}
-	}
-
-return;
-}
 
 void send_forged_dns_response(unsigned long destination_address, u_int16_t destination_port, \
 							char *data, u_short ip_length)
@@ -355,16 +225,11 @@ void print_ip_packet(const u_char *packet, u_char **payload, int *size_payload, 
 void
 got_packet(u_char *stringExpression, const struct pcap_pkthdr *header, const u_char *packet)
 {
-
-	static int count = 1;                   /* packet counter */
-	
 	/* declare pointers to packet headers */
 	const struct ether_header *ethernet;  /* The ethernet header [1] */
-	const struct sniff_ip *ip;              /* The IP header */
 
 	int size_payload;
 	u_char *payload;
-	count++;
 	
 	ethernet = (struct ether_header*)(packet);
 	
